9d6eeacb2e270361e505a2fcebd664daf4ab69d5
arougier <antoinerougier77@gmail.com>
2025-04-06 09:13:09 +0000

    premier API correct dans terminal

--- app/api.py
+++ app/api.py
@@ -20,10 +20,7 @@ n_process = max(1, cpu_count // 2)
 
 app = FastAPI()
 
-# Initialize the tokenizer
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-
-# Initialize the model
 modelTransformer = Transformer(
     pad_idx=0,
     voc_size=tokenizer.vocab_size,
@@ -35,7 +32,6 @@ modelTransformer = Transformer(
     n_layers=3,
 )
 
-# Load the model weights
 modelTransformer.load_state_dict(torch.load("model_weights/transformer_weights_25_epochs.pth"))
 modelTransformer.eval()
 
@@ -65,4 +61,15 @@ async def generate_summary(request: SummaryRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-# api works : curl -X POST "http://127.0.0.1:8000/generate_summary/" -H "Content-Type: application/json" -d '{"articles": ["new york police concerned drone tool terrorist investigate way stop potential attack police acknowledge drone potential weapon nypd say technology advance use carry air assault chemical weapon firearm police want develop technology allow control drone scan sky major event nypd say drone carry explosive number threat investigate way stop attack deputy chief salvatore dipace left concern incident year drone land german chancellor angela merkel take chancellor people drone fly pack football stadium manchester england week ago result suspect pilot arrest consult military member counterterrorism bomb squad emergency service aviation unit work plan counter weaponize drone nypd receive intelligence indicate imminent threat increasingly concerned year deputy chief salvatore dipace tell cbs news look people jury rig drone carry gun carry different type explosive want possibility worried mr dipace say police see video show accurate attack drone see video drone fly different target route accurately hit target paintball nypd see drone carry explosive number threat mr dipace concern follow incident germany year drone able land german chancellor angela merkel deliver speech drone circle land ms merkel deliver speech sin germany spark fear device easily commit terrorist act say think happen drone hit target right mark take chancellor people dramatic increase incident involve drone new york city year 40 record case unmanned aircraft system drone fly airspace nypd helicopter incident summer drone 800 foot ground nearly collide police helicopter nypd aviation unit member sergeant antonio hernandez say fly dark night vision goggle try job thing know drone come altitude"]}'
+''' api works : in Terminal 
+
+uvicorn app.api:app --reload
+curl -X POST "http://127.0.0.1:8000/generate_summary/" \
+     -H "Content-Type: application/json" \
+     -d '{
+           "articles": [
+             "new york police concerned drone tool terrorist investigate way stop potential attack police acknowledge drone potential weapon nypd say technology advance use carry air assault chemical weapon firearm police want develop technology allow control drone scan sky major event nypd say drone carry explosive number threat investigate way stop attack deputy chief salvatore dipace left concern incident year drone land german chancellor angela merkel take chancellor people drone fly pack football stadium manchester england week ago result suspect pilot arrest consult military member counterterrorism bomb squad emergency service aviation unit work plan counter weaponize drone nypd receive intelligence indicate imminent threat increasingly concerned year deputy chief salvatore dipace tell cbs news look people jury rig drone carry gun carry different type explosive want possibility worried mr dipace say police see video show accurate attack drone see video drone fly different target route accurately hit target paintball nypd see drone carry explosive number threat mr dipace concern follow incident germany year drone able land german chancellor angela merkel deliver speech drone circle land ms merkel deliver speech sin germany spark fear device easily commit terrorist act say think happen drone hit target right mark take chancellor people dramatic increase incident involve drone new york city year 40 record case unmanned aircraft system drone fly airspace nypd helicopter incident summer drone 800 foot ground nearly collide police helicopter nypd aviation unit member sergeant antonio hernandez say fly dark night vision goggle try job thing know drone come altitude"
+           ]
+         }'
+
+'''
